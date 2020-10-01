2039e2f8fdbbc1cd93c9f86c715fe97501613ed4
Stanislav Zyatyugin <szyatyugin@e1r5p18.unit.ua>
2020-10-01 17:38:30 +0300

    commit

--- build/app/header/headerEvents.ts
+++ build/app/header/headerEvents.ts
@@ -4,11 +4,38 @@ let get=(id:string):Element=>{
     return document.querySelector(id)
 }
 get('.header-navigation__menu').addEventListener('click',scrollToElement);
+get('.header-navigation__menu-button').addEventListener('click',showMenu);
+get('.header-button-close').addEventListener('click',closeMenu);
+window.addEventListener('scroll',showNavigation);
 
+//--- scroll to the assortment ---//
 function scrollToElement(e:any):void{
 if(e.target.textContent=='Coffee'){
  get('.section-assortment__info').scrollIntoView({block:'start',behavior:'smooth'})
 } 
+}
+//--- show the meny onclick event ---//
+function showMenu(e:any):void{
+if(get('.header-navigation__menu-button').classList.contains('active')){
+    get('.header-navigation__menu-button').classList.remove('active');
+}else{
+    get('.header-navigation__menu-button').classList.add('active');
+   
+}
+}
+//--- close the menu onclick event on button ---//
+function closeMenu():void{
+    if(get('.header-navigation__menu-button').classList.contains('active')){
+        get('.header-navigation__menu-button').classList.remove('active');
+    }
+}
+function showNavigation(e:any):void{
+    if(pageYOffset>100){
+    get('.header-navigation').classList.add('active');
+    }else{
+        get('.header-navigation').classList.remove('active');
+        get('.header-navigation__menu-button').classList.remove('active');
+    }
 }
 }
 headerEvents()
